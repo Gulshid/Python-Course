0fcad1cfaa24a6da56c53e85bc39fb08873fd580
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-05 19:10:38 +0500

    Error and Types of Error Done

---
+++ Python_basics/Ep_1/Error.py
@@ -0,0 +1,13 @@
+# Error and Types of Error
+
+# 1. Syntax Error
+
+print("Hello")
+print " Hello "
+
+# 2. Semantic Error
+length = 2, width = 3
+area = length * length # Incorrect
+area = length * width # correct 
+
+# 3. Runtime Error
