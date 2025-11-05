601c850acc770f012ef4b04e677dc30c2db48516
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-05 17:07:29 +0500

    print and input done

---
+++ Python_basics/Ep_1/Episode_1.py
@@ -0,0 +1,38 @@
+
+            # 1. introduction to Python
+            # 2. Installation of Python
+            # 3. Editors in Python
+            # 4. print and input Statement in Python
+            # 5. Variable and Constant in Python
+            # 6. Comments in Python
+            # 7. DataTypes in Python
+            # 8. Error and Types of Error in Python
+            # 9. Operator and Types of Operator in Python
+            # 10. Increment and Decrement in Python
+            # 11. Multiple way to Define Incre / Decre
+            # 12. Type Casting in Python
+            
+         # ****************** 
+         # 2 . Installation of Python
+         # 1. VS Code installed
+
+         # 2. Python installed on your system
+
+         # 3. The Python extension for VS Code
+         
+         
+         # ****************** 
+         
+         
+         # 3. Editors in Python
+         
+         
+            # ****************** 
+         
+         
+         # 4. print and input Statement in Python
+         
+         
+         
+
+            
---
+++ Python_basics/Ep_1/print_input.py
@@ -0,0 +1,15 @@
+# print Statemnt  ---> Display 
+# input Statement ----> USer input at RUntime / Console
+
+
+# print Statemnt  ---> Display 
+print("Asslam - u - Alikum ")
+print("HI! This is Python Programming language")
+
+
+
+# Input Statemnt  ---> USer input at RUntime / Console
+input_value = input("Enter any value for input :")
+
+
+
