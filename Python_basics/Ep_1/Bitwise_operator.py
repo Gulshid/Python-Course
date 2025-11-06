74eca010661bc64b9f2fed56ea0eeda5e8909292
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-06 07:23:12 +0500

    Bitwise operator done

---
+++ Python_basics/Ep_1/Bitwise_operator.py
@@ -0,0 +1,48 @@
+# Bitwise Operator
+# &, | , ^, ~
+
+a = 2 # 0010
+b = 5 # 0101
+
+# bitwsie &
+
+# 0010
+# 0101
+# --------
+# 0000 ====> Binary Number ===> Decimal ===> 0
+# ---------
+
+
+
+
+# bitwise |
+
+# 0010
+# 0101
+# --------
+# 0111 ==> Binary Number ====>Decimal ===> 7
+# ---------
+
+# bItwise ^
+
+# 0010
+# 0101
+# --------
+# 0111 ====> Binary Number ====>Decimal ===> 7
+# ---------
+
+# Bitwise ~ 
+
+# 0010 ===> 1101. ===> -3
+# 0101 ===> 1010 ===> -6
+
+print("Bitwise & :", (a & b))
+print("Bitwise | :", (a | b))
+print("Bitwise ^ :", (a ^ b))
+print("Bitwise ~ :", ~a)
+print("Bitwise ~ :", ~b)
+
+
+
+
+
