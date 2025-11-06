20fd233c2bc2db0226c6e384f3551bebaba4e16c
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-06 07:08:00 +0500

    Logical Operator done

---
+++ Python_basics/Ep_1/logical_operator.py
@@ -0,0 +1,16 @@
+# Logical Operator
+# and , or , not
+
+x = 6
+y = 2
+
+# Logical and 
+x > y and x == y ===> # x is greater or equal to y
+
+
+# Logical or
+
+x > y or x == y ===> # x is greater or equal to y
+
+# Logical not 
+not x > y  ===> # True ===> not False
