1dfa4ed0cb2f70763a65a9fc4064c9768d085301
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-08 14:54:03 +0500

    Multiple way to Increment and Decrement

---
+++ Python_basics/Ep_1/multiple_way_incre_decre.py
@@ -0,0 +1,16 @@
+# Multiple way to Increment and Decrement in python
+value = 5
+print(value)
+#  method 1 for Increment and  decremnt 
+value +=1
+print(value) # value = 6
+
+value -=1
+print(value) # value = 5
+
+# method 2 for Increment and Decrement
+value = value + 1
+print(value)  # value = 6
+
+value = value - 1
+print(value)   # value = 5
