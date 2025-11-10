2e3c8dcb3bba505dceaf7785c045cd1509c8b409
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-10 06:58:33 +0500

    Additon of two value is Done

---
+++ Python_basics/Ep_1/Task/String_value.py
@@ -0,0 +1,3 @@
+# 1. How to Display a String Value
+print("Hello World ", end=" ")
+print("This is First Task of Chapter 1 of Python")
---
+++ Python_basics/Ep_1/Task/add_two_value.py
@@ -0,0 +1,20 @@
+a = 4
+b = 5
+
+# Method 1. Without third variable
+print(f"The additon of two value is : {a + b}")
+
+# Method 2. With Third Variable
+c = a + b
+print(f"The Addition of two value is : {c}")
+
+
+x = int(input("Enter the value for x :"))
+y = int(input("Enter the value for y :"))
+
+# Method 1. Without third variable
+print(f"The additon of two value is : {x + y}")
+
+# Method 2. With Third Variable
+z = x + y
+print(f"The additon of two value is : {z} ")
