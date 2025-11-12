7e4f6b1e8ac26ad67f833cf5c4d40f74f95611b4
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-12 07:20:07 +0500

    Datatype task done

---
+++ Python_basics/Ep_1/Task/datatype_value.py
@@ -0,0 +1,45 @@
+# Write a Program that can print all Datatype value
+# Int Input 
+a = int(input("Enter the value for integer datatype :"))
+
+# float Input
+b = float(input("Enter the value for float datatype :"))
+
+# String Input
+c = str(input("Enter the value for String Datatype :"))
+
+# Boolean Input
+d = bool(input("Enter the value for Boolean Datatype :"))
+
+# List Input
+e = list(input("Enter the value for list Datatype :"))
+
+# Tuple Input
+f = tuple(input("Enter the value for tuple Datatype :"))
+
+# Set Input
+g = set(input("Enter the value for Set Datatype :"))
+
+# Dict Input
+h = input("Enter the value for Dictionary Datatype :")
+
+
+print(f"The Int Datatype : {a}")
+
+print(f"The Float Datatype : {b}")
+
+print(f"The String Datatype : {c}")
+
+print(f"The Boolean Datatype : {d}")
+
+
+print(f"The List Datatype : {e}")
+
+print(f"The Tuple Datatype : {f}")
+
+print(f"The Set Datatype : {g}")
+
+print(f"The Dict  Datatype : {h}")
+
+
+
---
+++ Python_basics/Ep_1/Task/greeting.py
@@ -0,0 +1,2 @@
+greet = str(input("Enter the greeting Message :"))
+print("The Greeting Message is :",greet)
