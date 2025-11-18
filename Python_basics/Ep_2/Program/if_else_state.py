f5b3d539d4cef593611a728d58e566235932c379
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-18 23:30:03 +0500

    if and if -else done

---
+++ Python_basics/Ep_2/Program/if_else_state.py
@@ -0,0 +1,14 @@
+# y = 3
+# if condition:
+#     statement
+    
+    
+# else:
+#     statement
+
+c = 21
+if c == 20:
+    print("The value is equal to 20 ")
+    
+else:
+    print("The value is not equal to 20")
