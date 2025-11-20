ec9de70747cab4a5ee551143cc7fade3f85bbb67
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-20 13:21:13 +0500

    Python Chapter 2 Completed--->

---
+++ Python_language/Ep_2/Program/nested_loop.py
@@ -0,0 +1,6 @@
+#  concept of Nested loop
+for a in range(5): # outer loop ---> row 
+    for b in range(6): # inner loop ---> col
+        print("*", end= " ")
+    print()
+        
---
+++ Python_language/Ep_2/Program/while_loop.py
@@ -0,0 +1,12 @@
+#  Concept of while loop
+# a = starting point
+# while condition: ---> ending point
+    # statement
+    # increment / decrement
+
+
+# 1 ----> 20
+val = 1
+while val <= 20:
+    print(val, end = " ")
+    val = val + 1
