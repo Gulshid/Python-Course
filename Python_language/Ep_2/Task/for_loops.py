628c27114987aa4f675bd36a5e7de3b56d986931
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-21 19:24:37 +0500

    Task 2 Completed

---
+++ Python_language/Ep_2/Task/for_loops.py
@@ -0,0 +1,12 @@
+#3. Find even & odd numbers using list slicing / step / modulus (second method)
+print("Even Numbers :")
+for even in range(0, 21, 2):
+    print(even, end= " ")
+
+print()
+
+print("Odd Number :")
+for odd in range(1, 21, 2):
+    print(odd, end= " ")
+    
+print()
