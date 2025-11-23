281dfcfdd5d60c74d0594025c231d231c129401e
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-23 10:36:59 +0500

    ASCII task done

---
+++ Python_language/Ep_2/Task/ASCII.py
@@ -0,0 +1,9 @@
+# 9. ASCII Code
+
+    # 1.Find ASCII code of any character using ord()
+    
+char = str(input("Enter ANy Character from Keyboard :"))
+
+Ascii = ord(char)
+
+print(f"The Character {char} and Their Ascii Code : {Ascii}")
