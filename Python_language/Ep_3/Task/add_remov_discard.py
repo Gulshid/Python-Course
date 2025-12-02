1f4c6393543459c7efbe6f2850662b9cb86cb9ea
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-02 14:15:28 +0500

    Task 1 and 2 completed

---
+++ Python_language/Ep_3/Task/add_remov_discard.py
@@ -0,0 +1,20 @@
+# Task 2: Add/remove elements (add, remove, discard)
+my_set = {2,4,6}
+
+print("Set values :")
+print(my_set)
+
+print("************")
+print("add Operation in set :")
+my_set.add(8)
+print(my_set)
+
+print("************")
+print("Remove Operation in set : ")
+my_set.remove(4)
+print(my_set)
+
+print("************")
+print('Discard Operation in set :')
+my_set.discard(10)
+print(my_set)
---
+++ Python_language/Ep_3/Task/create_display_set.py
@@ -0,0 +1,13 @@
+# Task 1: Create a set and display it
+my_set = {1,2,3,4,5,6}
+
+# display the set 
+print("Set  values :")
+print(my_set)
+
+# display set using loop
+print("***************")
+print("Set values using loop :")
+for val in my_set:
+    print(val, end = " ")
+print()
