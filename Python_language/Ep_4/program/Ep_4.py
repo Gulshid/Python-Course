4bd5efaaa7706db9c27baa0abf69a7f51d09802a
Gulshid Zada <gulshidzada4@gmail.com>
2025-12-04 07:36:38 +0500

    function part explain

---
+++ Python_language/Ep_4/program/Ep_4.py
@@ -0,0 +1,21 @@
+
+                    #    *** Chapter 4. Function ***
+
+# 1. Introduction to Functions – Understanding what functions are and why we use them.
+        # 1.Reuseibility  2. modularity  3. readibility
+
+# 2. Defining a Function – Learning how to create a function using the def keyword.
+
+# 3. Calling a Function – How to execute a function after defining it.
+
+# 4. Function Parameters – Passing values into a function.
+
+# 5. Return Statement – Sending a value back from a function.
+
+# 6. Local and Global Variables – Understanding variable scope inside functions.
+
+# 7. ***args and kwargs – Using variable-length arguments in functions.
+
+# 8. Recursive Functions – Functions that call themselves.
+
+# 9. Lambda Functions – Writing small anonymous one-line functions.
---
+++ Python_language/Ep_4/program/create_function.py
@@ -0,0 +1,9 @@
+# lecture 2. Defining a Function – Learning how to create a function using the def keyword.
+def display(n):   # 1. function Declaration {def , function name, parameters}
+    print("My name is ", n)  # 2.function Definition {body of function}
+    
+
+# 3. Calling a Function – How to execute a function after defining it.
+# 3.function call 
+display("Gulshid Zada")
+    
